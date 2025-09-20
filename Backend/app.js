8cb065ca446afc8f86ae01c506cea58913a6cf10
Rajbanshiprashant <prashantrajbanshi0@gmail.com>
2025-09-20 22:30:56 +0545

    database connected mongo db

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/mydatabase', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+  .then(() => {
+    console.log('MongoDB connected');
+    
+    // Start the Express server after a successful connection to MongoDB
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+// Basic route
 app.get('/', (req, res) => {
   res.send('Ankit is a good guy');
-})
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
